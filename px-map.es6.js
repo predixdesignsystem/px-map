0b43fc41fce85de4184b6419149c7613d5d65e54
David Leonard <davidrobertleonard@gmail.com>
2017-02-14 16:11:52 -0800

    Add hover state for zoom control buttons

--- px-map.es6.js
+++ px-map.es6.js
@@ -267,10 +267,10 @@
       }
 
       // Ensure a tile layer is applied to the map
-      // L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      //   attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery &copy; <a href="http://mapbox.com">Mapbox</a>',
-      //   maxZoom: 18
-      // }).addTo(this.mapInstance);
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery &copy; <a href="http://mapbox.com">Mapbox</a>',
+        maxZoom: 18
+      }).addTo(this.mapInstance);
 
       // Set the view from current defaults
       this._updateMapView();
