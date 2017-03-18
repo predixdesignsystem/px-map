a1a5c6dabd3de5198d0806d534ce9e08b0318dea
David Leonard <davidrobertleonard@gmail.com>
2017-03-17 18:04:56 -0700

    Unwrap non-native event that causes failure in FF/etc., fixes #18

--- px-map-behavior-marker-group.es6.js
+++ px-map-behavior-marker-group.es6.js
@@ -446,11 +446,8 @@
      * it is not kept around as an opaque background to the markers.
      */
     _handleClusterSpiderify(evt) {
-      const localEvt = Polymer.dom(evt);
-      const node = localEvt.node;
-      if (!node || !node.cluster || !node.cluster._icon) return;
-
-      node.cluster._icon.style.visibility = 'hidden';
+      if (!evt || !evt.cluster || !evt.cluster._icon) return;
+      evt.cluster._icon.style.visibility = 'hidden';
     },
 
     /**
@@ -458,11 +455,8 @@
      * it is shown again on the map.
      */
     _handleClusterUnspiderify(evt) {
-      const localEvt = Polymer.dom(evt);
-      const node = localEvt.node;
-      if (!node || !node.cluster || !node.cluster._icon) return;
-
-      node.cluster._icon.style.visibility = 'visible';
+      if (!evt || !evt.cluster || !evt.cluster._icon) return;
+      evt.cluster._icon.style.visibility = 'visible';
     }
   };
   /* Bind MarkerGroup behavior */
