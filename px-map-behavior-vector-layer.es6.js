df3c2484233801142e3b4b5a5aa1bd178674a44f
David Fox <david.fox@ge.com>
2017-04-26 17:18:35 +0100

    Points are now styled as CircleMarkers and default styling is supplied. Example data expanded to include lines. Properties can optionally be shown in a popover.

--- px-map-behavior-vector-layer.es6.js
+++ px-map-behavior-vector-layer.es6.js
@@ -15,13 +15,33 @@
   PxMapBehavior.VectorLayerImpl = {
     properties: {
       /**
-       * A string that will be contain the geojson:  (or point to the geojson source)??
+       * A string that will be contain the geojson:
        *
        * @type {String}
        */
       data: {
         type: String,
         observer: 'shouldUpdateInst'
+      },
+
+      /**
+       * An object that can specify the style
+       *
+       * @type {Object}
+       */
+      featureStyle: {
+        type: Object,
+        value: null
+      },
+
+      /**
+       * An property that if set will show a popover containing all the feature properties when the feature is clicked
+       *
+       * @type {Boolean}
+       */
+      showFeatureProperties: {
+        type: Boolean,
+        value: false
       }
     },
 
@@ -30,7 +50,30 @@
     },
 
     createInst(options) {
-      return L.geoJson(JSON.parse(options.data));
+      return L.geoJson(JSON.parse(options.data), {
+        pointToLayer: function(feature, latlng) {
+          return new L.CircleMarker(latlng, {
+            radius: options.featureStyle.radius           || 5,
+            color: options.featureStyle.color             || '#3E87E8', //primary-blue,
+            fillColor: options.featureStyle.fillColor     || '#88BDE6', //$dv-light-blue
+            weight: options.featureStyle.weight           || 2,
+            opacity: options.featureStyle.opacity         || 1,
+            fillOpacity: options.featureStyle.fillOpacity || 0.4
+          })
+        },
+        onEachFeature: (feature, layer) => {
+          if(!this.showFeatureProperties) return;
+          let popupString = '';
+
+          Object.keys(feature.properties).forEach( function(value) {
+            if(feature.properties[value] && feature.properties[value] !== 'unset') {
+              popupString += value + ' : ' + feature.properties[value] +  '<br>';
+            }
+          });
+
+          layer.bindPopup(popupString);
+        }
+      });
     },
 
     updateInst(lastOptions, nextOptions) {
@@ -42,7 +85,8 @@
 
     getInstOptions() {
       return {
-        data: this.data
+        data: this.data,
+        featureStyle: this.featureStyle
       };
     }
   };
