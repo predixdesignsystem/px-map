3f2fd8622b1a2f45116b58c7ede66dad3875b424
David Leonard <davidrobertleonard@gmail.com>
2017-04-28 16:41:01 -0700

    Add option to force tile layer URL to be decoded before passing to tile service.

---
+++ test/px-map-tile-layer-tests.js
@@ -0,0 +1,26 @@
+document.addEventListener("WebComponentsReady", function() {
+  runCustomTests();
+});
+
+function runCustomTests() {
+
+  describe('px-map-tile-layer', function () {
+    var sandbox;
+
+    beforeEach(function () {
+      sandbox = sinon.sandbox.create();
+    });
+
+    afterEach(function () {
+      sandbox.restore();
+    });
+
+    it('decodes its `url` attribute if `decodeUrl` is enabled', function() {
+      var encodedTileEl = fixture('TileLayerEncodedUrlFixture');
+      var encodedUrl = encodedTileEl.url;
+      var options = encodedTileEl.getInstOptions();
+      expect(options.url).to.equal('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
+    });
+  });
+
+}
