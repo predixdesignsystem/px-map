ea8eba6a20e9f6d1d7a145f15b6735b2487166fa
Tali Marcus <tali.marcus@ge.com>
2017-08-31 18:16:25 -0700

    WIP: tests for marker group

--- test/px-map-marker-group-tests.js
+++ test/px-map-marker-group-tests.js
@@ -18,6 +18,25 @@ function runCustomTests() {
       sandbox.restore();
     });
 
+    it('adds custom color types to the colorsByType object', function(done) {
+      setTimeout(function() {
+        var colorsByType = markerGroup.colorsByType;
+        var keys = Object.keys(colorsByType);
+        expect(keys.includes('custom-0')).to.equal(true);
+        expect(keys.includes('custom-1')).to.equal(true);
+        expect(keys.includes('custom-2')).to.equal(true);
+        done();
+      }, 2000);
+    });
+
+    it('sends custom cluster colors down to individual markers', function(done) {
+      setTimeout(function() {
+        // do something to make _createMarkerIcon be called with a custom icon type
+        // check that when _createMarkerIcon is called, options is updated with
+        // the custom color of the icon
+        done();
+      }, 2000);
+    });
 
   });
 
